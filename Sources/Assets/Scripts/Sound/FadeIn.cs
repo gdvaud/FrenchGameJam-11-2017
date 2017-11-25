82d16b87d78eddb98f580e085b2dc66ccaf91882
Guillaume <gdvaud@gmail.com>
2017-11-25 23:20:22 +0100

    Score beans and princess

--- Sources/Assets/Scripts/Sound/FadeIn.cs
+++ Sources/Assets/Scripts/Sound/FadeIn.cs
@@ -5,19 +5,21 @@ using UnityEngine;
 public class FadeIn : MonoBehaviour {
 
     public AudioSource sound;
+    private float initialVolume;
     private float timeStart;
     public float fadeDuration;
 
     private void Start() {
         timeStart = Time.time;
+        initialVolume = sound.volume;
     }
 
     // Update is called once per frame
     private void Update() {
-        if (sound.volume == 1) {
+        sound.volume = Mathf.Lerp(0, initialVolume, (Time.time - timeStart) / fadeDuration);
+
+        if (sound.volume >= initialVolume) {
             Destroy(this);
         }
-        Debug.Log("123");
-        sound.volume = Mathf.Lerp(0, 1, (Time.time - timeStart) / fadeDuration);
     }
 }
