5d634882dd3a69ce8457a923415a3a735493abd4
Guillaume <gdvaud@gmail.com>
2017-11-25 15:34:36 +0100

    Tank Generation Bis

--- Sources/Assets/Scripts/GameManager.cs
+++ Sources/Assets/Scripts/GameManager.cs
@@ -10,18 +10,33 @@ public class GameManager : MonoBehaviour {
     public UIManager UIManager;
     private Dictionary<int, TankManager> tanks;
 
-    [Header("Game Variable")]
+    [Header("Tank Generation")]
     public int nbPlayer = 2;
+    public int maxPlayer = 8;
+    private List<int> unusedAreas;
     public GameObject tankPrefab;
+    public Transform minLocation;
+    public Transform maxLocation;
+    private float spawnWidth;
 
     private void Awake() {
         tanks = new Dictionary<int, TankManager>();
+        unusedAreas = new List<int>();
+        for (int i = 0; i < maxPlayer; i++) {
+            unusedAreas.Add(i);
+        }
     }
     // Use this for initialization
     void Start() {
+        spawnWidth = maxLocation.position.x - minLocation.position.x;
 
         for (int i = 1; i <= nbPlayer; i++) {
             GameObject tank = GameObject.Instantiate(tankPrefab);
+
+            Vector2 tankPosition = tank.transform.position;
+            tankPosition.x = generatePosition() + minLocation.position.x;
+            Debug.Log(tankPosition);
+            tank.transform.position = tankPosition;
 
             TankManager tankManager = tank.GetComponent<TankManager>();
             tankManager.setGameManager(this);
@@ -37,5 +52,10 @@ public class GameManager : MonoBehaviour {
         TankManager tank;
         tanks.TryGetValue(player, out tank);
         tank.setIsLoading(state);
+    }
+
+    private float generatePosition() {
+        int area = Mathf.RoundToInt(Random.Range(0, unusedAreas.Count));
+        return (spawnWidth / maxPlayer * area) + (spawnWidth / maxPlayer / 2);
     }
 }
