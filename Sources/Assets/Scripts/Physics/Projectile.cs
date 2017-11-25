51ddd0e40f8f17b9becd900c92802f2d868c035f
Simon <simon@brulhart.me>
2017-11-25 14:52:23 +0100

    Move projectile gameplay logic into ProjectileManager

--- Sources/Assets/Scripts/Physics/Projectile.cs
+++ Sources/Assets/Scripts/Physics/Projectile.cs
@@ -10,14 +10,9 @@ public class Projectile : MonoBehaviour {
 
     private Vector3 v;
     private Vector3 accel = new Vector3(0f, -9.81f);
-    private const float NOCLIP_DURATION = 1f;
-    private float creationTime;
-    private Collider2D collider;
 
 	// Use this for initialization
 	void Start () {
-        creationTime = Time.time;
-        collider = GetComponent<Collider2D>();
         var angle = transform.eulerAngles.z * Math.PI / 180f;
         v = new Vector3(
             force * (float) Math.Cos(angle),
@@ -25,23 +20,12 @@ public class Projectile : MonoBehaviour {
         );
 	}
 
-
 	// Update is called once per frame
 	void Update () {
         var dt = Time.deltaTime;
         transform.position += accel * (dt * dt) / 2f + v * dt;
         v += accel * dt;
-
-        // Enable collider after tolerance duration exceeded
-        if (!collider.enabled && Time.time - creationTime > NOCLIP_DURATION) {
-            collider.enabled = true;
-        }
 	}
-
-    void OnTriggerEnter2D(Collider2D coll) {
-        Debug.Log("Collision", this);
-        Destroy(gameObject);
-    }
 
     public void setForce(float force) {
         this.force = force;
--- Sources/Assets/Scripts/ProjectileManager.cs
+++ Sources/Assets/Scripts/ProjectileManager.cs
@@ -4,13 +4,27 @@ using UnityEngine;
 
 public class ProjectileManager : MonoBehaviour {
 
+    private const float NOCLIP_DURATION = 1f;
+    private float creationTime;
+
 	// Use this for initialization
 	void Start () {
-		
+        creationTime = Time.time;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    void OnTriggerEnter2D(Collider2D coll) {
+        // Enable collider after tolerance duration exceeded
+        if (Time.time - creationTime < NOCLIP_DURATION) {
+            Debug.Log("Collision (ignored)", this);
+        } else {
+            Debug.Log("Collision", this);
+            Destroy(gameObject);
+        }
+    }
+
 }
