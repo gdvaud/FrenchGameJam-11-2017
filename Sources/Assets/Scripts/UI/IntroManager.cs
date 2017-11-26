093f8a101251ee42f7f46f5c63945a6411414ee1
Simon <simon@brulhart.me>
2017-11-26 19:29:47 +0100

    Add Intro

---
+++ Sources/Assets/Scripts/UI/IntroManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IntroManager : MonoBehaviour {
+
+    public ScenesManager scenesManager;
+    public AudioSource jingle;
+    public SpriteRenderer startScreen;
+
+	// Use this for initialization
+	void Start () {
+        jingle.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var color = startScreen.color;
+        color.a += 0.015f;
+        startScreen.color = color;
+        if (!jingle.isPlaying) {
+            scenesManager.loadMenu();
+        }
+	}
+}
